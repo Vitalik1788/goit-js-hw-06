d6e8851439215632ac5faa1837c788a073665388
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-02-26 14:03:22 +0200

    done task 09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  button: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+};
+
+refs.button.addEventListener('click', onButtonClickChangeColor);
+
+function onButtonClickChangeColor(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = document.body.style.backgroundColor;
+  
+};
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
