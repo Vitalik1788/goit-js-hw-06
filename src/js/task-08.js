a5f7e5899e6a1f686f87152957d59fe3bd021ce0
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-02-26 13:07:00 +0200

    done task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,34 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formTargetEl = event.currentTarget.elements;
+  let email = ''; 
+  let password = '';
+
+  if (formTargetEl.email.value === '') {
+    alert('необходимо заполнить поле Email');
+    return;
+  } else {
+    email = formTargetEl.email.value;
+  };
+
+  if (formTargetEl.password.value === '') {
+    alert('необходимо заполнить поле Password');
+    return;
+  } else {
+    password = formTargetEl.password.value;
+  };
+   
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  formEl.reset();
+  
+}
